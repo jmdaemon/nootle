d8193470503a5daca8f421071f15c492c5c1aa7c
Joseph Diza <josephm.diza@gmail.com>
2022-04-15 00:27:51 -0700

    Add initial new note dialog button
    The callback for the button doesn't work properly

--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,69 @@
 import React from 'react';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import TextField from '@mui/material/TextField';
 import './Button.css';
 
+// Form Dialog
+//export function FormDialog() {
+  //const [open, setOpen] = React.useState(false);
+
+  //const handleClickOpen = () => {
+    //setOpen(true);
+  //};
+
+  //const handleClose = () => {
+    //setOpen(false);
+  //}
+//};
+
+//class FormDialog {
+  //constructor(open) {
+    //this.open = open;
+  //}
+
+  //handleClickOpen() {
+    //this.open = true;
+  //}
+
+  //handleClose() {
+    //this.open = false;
+  //}
+//}
+
 class Menu extends React.Component {
-    constructor(props) {
-      super(props);
-      this.notes = props.notes
-    }
-  
-  handleNewNote() {
-    console.log("Button was clicked");
+  constructor(props) {
+    super(props);
+    this.notes = props.notes
+    //this.formDialog = FormDialog();
+    //this.formDialog = new FormDialog(false);
+
+    //const [open, setOpen] = React.useState(false);
+    //const [open, setOpen] = React.useState(true);
+    //this.open = open;
+    //this.setOpen = setOpen;
+    this.open = true;
   }
+
+  setOpen(value) {
+    this.open = value;
+  }
+
+  handleCreateNewNote() {
+    this.setOpen(true);
+  }
+
+  handleCancelNewNote() {
+    this.setOpen(false);
+  }
+  
+  //handleNewNote() {
+    //this.formDialog.handleClickOpen();
+    ////console.log("Button was clicked");
+  //}
 
   render() {
     const notesList = this.notes.map((note) => <li>{note}</li>);
@@ -20,7 +73,24 @@ class Menu extends React.Component {
         <ul>
           {notesList}
         </ul>
-        <Button onClick={this.handleNewNote}>+</Button>
+        <Button onClick={this.handleCreateNewNote}>+</Button>
+        <Dialog open={this.open} onClose={this.handleCancelNewNote}>
+        <DialogTitle>Make New Note</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="name"
+            label="File name of your new note"
+            fullWidth
+            variant="standard"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.handleCancelNewNote}>Cancel</Button>
+          <Button onClick={this.handleCancelNewNote}>Create</Button>
+        </DialogActions>
+      </Dialog>
       </div>
     );
   }
