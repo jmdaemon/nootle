06c48b0d0f62168f9dcd91085adfa12528244365
Joseph Diza <josephm.diza@gmail.com>
2022-04-14 22:52:39 -0700

    Make Menu a react component

--- src/Menu.js
+++ src/Menu.js
@@ -1,10 +1,13 @@
+import React from 'react';
 
-function Menu() {
-  return (
-    <div>
-      <h2>Your Nootles</h2>
-    </div>
-  );
+class Menu extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Your Nootles</h2>
+      </div>
+    );
+  }
 }
 
 export default Menu;
